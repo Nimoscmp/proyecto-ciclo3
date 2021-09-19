faa75f797ee33be014adb6bfd7d6014760334158
nimos <nimos.cmp@gmail.com>
2021-09-19 17:23:06 -0500

    Add header component

---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+  <header class="header">
+    <img src="" alt="logo">
+    
+    <h2>Subtítulo</h2>
+    
+    <nav>
+      <ul>
+        <li>Quienes somos</li>
+        <li>Servicios</li>
+        <li>Testimonios</li>
+        <li>Contacto</li>
+      </ul>
+    </nav>
+
+    <button type="button">Ingresar</button>
+  </header>
+</template>
