aa9c126aecd86e1ade4e1cc5bddb5b5899324bfa
Nimoscmp <nimos.cmp@gmail.com>
2021-09-26 22:42:20 -0500

    Complete reviews html

--- src/components/Reviews.vue
+++ src/components/Reviews.vue
@@ -1,5 +1,40 @@
 <template>
     <section class="section reviews">
-        
+        <div class="reviews__text">
+            <h3 class="h3 reviews__subtitle">
+                Testimonios
+            </h3>
+            <h2 class="h2 reviews__title">
+                Algunos testimonios
+            </h2>
+            <p class="text reviews__info">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sed praesentium est accusamus iure rerum facere aspernatur rem modi, iste voluptas aperiam possimus expedita dicta fugiat voluptate quas. Illo, quam repellendus?</p>
+        </div>
+
+        <div class="reviews__wrapper">
+            <div class="reviews__card">
+                <p class="text reviews__card-text">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi sapiente libero repellendus facere nostrum, doloribus sed beatae unde inventore labore. Labore dignissimos sint impedit ipsa aliquam nam accusamus nemo qui!</p>
+                <span class="reviews__card-icon"></span>
+                <span class="reviews__card-name">Name LastName</span>
+                <span class="reviews__card-position">Position</span>
+            </div>
+            <div class="reviews__card">
+                <p class="text reviews__card-text">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi sapiente libero repellendus facere nostrum, doloribus sed beatae unde inventore labore. Labore dignissimos sint impedit ipsa aliquam nam accusamus nemo qui!</p>
+                <span class="reviews__card-icon"></span>
+                <span class="reviews__card-name">Name LastName</span>
+                <span class="reviews__card-position">Position</span>
+            </div>
+            <div class="reviews__card">
+                <p class="text reviews__card-text">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi sapiente libero repellendus facere nostrum, doloribus sed beatae unde inventore labore. Labore dignissimos sint impedit ipsa aliquam nam accusamus nemo qui!</p>
+                <span class="reviews__card-icon"></span>
+                <span class="reviews__card-name">Name LastName</span>
+                <span class="reviews__card-position">Position</span>
+            </div>
+            <div class="reviews__card">
+                <p class="text reviews__card-text">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi sapiente libero repellendus facere nostrum, doloribus sed beatae unde inventore labore. Labore dignissimos sint impedit ipsa aliquam nam accusamus nemo qui!</p>
+                <span class="reviews__card-icon"></span>
+                <span class="reviews__card-name">Name LastName</span>
+                <span class="reviews__card-position">Position</span>
+            </div>
+        </div>
     </section>
 </template>
