77c9ea14d75523847cc15281006045ad1263a858
Nimoscmp <nimos.cmp@gmail.com>
2021-09-26 21:47:52 -0500

    Modify header html

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,18 +1,28 @@
 <template>
   <header class="header">
-    <img src="" alt="logo">
+    <img src="" alt="logo" class="header__logo">
     
     <h2>Subtítulo</h2>
     
-    <nav>
-      <ul>
-        <li>Quienes somos</li>
-        <li>Servicios</li>
-        <li>Testimonios</li>
-        <li>Contacto</li>
+    <nav class="header__nav">
+      <ul class="header__list">
+        <li class="header__list-item">Quienes somos</li>
+        <li class="header__list-item">Servicios</li>
+        <li class="header__list-item">Testimonios</li>
+        <li class="header__list-item">Contacto</li>
       </ul>
     </nav>
 
     <button type="button">Ingresar</button>
   </header>
 </template>
+
+<script>
+
+</script>
+
+
+
+<style scoped>
+
+</style>
