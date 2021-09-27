0d8c7a5c871b21c6008cb5c1711cad54c79285dc
Nimoscmp <nimos.cmp@gmail.com>
2021-09-26 22:47:04 -0500

    Complete footer html

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,20 @@
 <template>
     <footer class="footer">
-        
+        <div class="footer__info">
+            <h2 class="h2 h2--mini footer__info-title">
+                JavaLovers
+            </h2>
+            <p class="text footer__info-text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Autem minus quisquam provident possimus vitae laborum vero eius consectetur reiciendis facere doloribus voluptate dolor aut nam, quos beatae harum quaerat obcaecati?</p>
+        </div>
+        <div class="footer__social">
+            <h3 class="h3 footer__social-title">
+                Nuestras redes
+            </h3>
+            <div class="footer__social-icons">
+                <span class=""></span>
+                <span class=""></span>
+                <span class=""></span>
+            </div>
+        </div>
     </footer>
 </template>
