6112e834abd7db373d2ab6910b2d70406eb1c5bc
Nimoscmp <nimos.cmp@gmail.com>
2021-09-27 21:00:04 -0500

    Create header for mobile

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,19 +1,21 @@
 <template>
-  <header class="header">
-    <img src="" alt="logo" class="header__logo">
+  <header class="header header-desktop">
+    <img src="" alt="logo" class="header-desktop__logo">
     
-    <h2>Subtítulo</h2>
+    <h2 class="h2 header-desktop__title">
+      Subtítulo
+    </h2>
     
-    <nav class="header__nav">
-      <ul class="header__list">
-        <li class="header__list-item">Quienes somos</li>
-        <li class="header__list-item">Servicios</li>
-        <li class="header__list-item">Testimonios</li>
-        <li class="header__list-item">Contacto</li>
+    <nav class="header-desktop__nav">
+      <ul class="header-desktop__list">
+        <li class="header-desktop__list-item">Quienes somos</li>
+        <li class="header-desktop__list-item">Servicios</li>
+        <li class="header-desktop__list-item">Testimonios</li>
+        <li class="header-desktop__list-item">Contacto</li>
       </ul>
     </nav>
 
-    <button type="button" class="btn btn--secondary header__btn">Ingresar</button>
+    <button type="button" class="btn btn--secondary header-desktop__btn">Ingresar</button>
   </header>
 </template>
 
