fb6c5a1d5dbb0964491af3f654081b074057eb05
Nimoscmp <nimos.cmp@gmail.com>
2021-10-10 13:09:23 -0500

    Advance in handle login

--- src/components/PopUp.vue
+++ src/components/PopUp.vue
@@ -13,8 +13,22 @@
       action="" 
       class="form" 
       v-on:submit.prevent="handleLogin">
-      <input type="email" name="email" id="popup-email" class="input popup__input" placeholder="Ingresa tu email">
-      <input type="password" name="password" id="popup-password" class="input popup__input" placeholder="Ingresa tu contraseña">
+      <input 
+        type="email" 
+        name="email" 
+        id="popup-email" 
+        class="input popup__input" 
+        placeholder="Ingresa tu email"
+        v-model="email"  
+      >
+      <input 
+        type="password" 
+        name="password" 
+        id="popup-password" 
+        class="input popup__input" 
+        placeholder="Ingresa tu contraseña"
+        v-model="password"  
+      >
       <button 
         type="submit" 
         class="btn btn--primary popup__btn"
@@ -42,7 +56,8 @@ export default {
       linkText: '¿No estás registrado?',
       baseUrl: 'https://backend-project-mintic7.herokuapp.com/api/',
       email: '',
-      password: ''
+      password: '',
+      responseData: {}
     };
   },
   methods: {
@@ -53,19 +68,34 @@ export default {
     },
     handleLogin() {
       let base_url
+      let _this = this
 
-      const fetchData = async() => {
-        const response 
+      const fetchData = async(url) => {
+
+        const completeUrl = `${ url }${ _this.email }&${_this.password}`
+
+        const response = await fetch(completeUrl, {
+          headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': 'http://localhost:8080/'
+          }
+        })
+        const json_response = await response.json()
+
+        _this.responseData = json_response
+        console.log(_this.responseData)
       } 
 
       //  Login
       if(this.btnString === 'Ingresar') {
-        base_url = `${ this.baseUrl }/login/`
+        base_url = `${ this.baseUrl }/login?`
+        fetchData(base_url)
       } 
       
       //  Register
       else {
-
+        base_url = `${ this.baseUrl }/register?`
+        fetchData(base_url)
       }
     },
     handleToRegister(e) {
